5942ad783cd69554701f3d6e39f41ea411fcf979
Flávio Azevedo Neves Amarante <36772257+FlavioAmarante@users.noreply.github.com>
2026-06-16 17:01:27 -0300

    Add data accumulation function with FeO
    This function accumulates data in a DataFrame based on specified break variables and conditions, initializing several columns to NaN and then calculating accumulated values based on conditions.

---
+++ Iron_ore/ddb_acc_feo.py
@@ -0,0 +1,168 @@
+# Acumulacao banco de dados
+def ddb_acc_feo(df1,varbreak,brk):
+    
+    import pandas as pd
+    import numpy as np
+    
+    var_break = varbreak.lower()
+    
+    df = df1.copy()
+    df = df.rename(columns=str.lower)
+    
+    # ZERA VARIAVEIS ACUMULADAS
+    
+    df['afe1'] = np.nan
+    df['asi1'] = np.nan
+    df['ap1'] = np.nan
+    df['aal1'] = np.nan
+    df['amn1'] = np.nan
+    df['apf1'] = np.nan
+    df['ati1'] = np.nan
+    df['amg1'] = np.nan
+    df['aca1'] = np.nan
+    df['afe3o4_1'] = np.nan
+
+    df['afe2'] = np.nan
+    df['asi2'] = np.nan
+    df['ap2'] = np.nan
+    df['aal2'] = np.nan
+    df['amn2'] = np.nan
+    df['apf2'] = np.nan
+    df['ati2'] = np.nan
+    df['amg2'] = np.nan
+    df['aca2'] = np.nan
+    df['afe3o4_2'] = np.nan
+
+    df['afe3'] = np.nan
+    df['asi3'] = np.nan
+    df['ap3'] = np.nan
+    df['aal3'] = np.nan
+    df['amn3'] = np.nan
+    df['apf3'] = np.nan
+    df['ati3'] = np.nan
+    df['amg3'] = np.nan
+    df['aca3'] = np.nan
+    df['afe3o4_3'] = np.nan
+
+    df['afe4'] = np.nan
+    df['asi4'] = np.nan
+    df['ap4'] = np.nan
+    df['aal4'] = np.nan
+    df['amn4'] = np.nan
+    df['apf4'] = np.nan
+    df['ati4'] = np.nan
+    df['amg4'] = np.nan
+    df['aca4'] = np.nan
+    df['afe3o4_4'] = np.nan
+
+    # ACUMULACAO
+    
+    mask = (df[var_break] == brk) & (df['g1'] > 0) 
+    
+    df.loc[mask, 'afe1'] = (np.nan_to_num(df.loc[mask, 'fe1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'asi1'] = (np.nan_to_num(df.loc[mask, 'si1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'ap1'] = (np.nan_to_num(df.loc[mask, 'p1'] * df.loc[mask, 'g1'])    )
+    df.loc[mask, 'aal1'] = (np.nan_to_num(df.loc[mask, 'al1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'amn1'] = (np.nan_to_num(df.loc[mask, 'mn1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'apf1'] = (np.nan_to_num(df.loc[mask, 'pf1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'ati1'] = (np.nan_to_num(df.loc[mask, 'ti1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'amg1'] = (np.nan_to_num(df.loc[mask, 'mg1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'aca1'] = (np.nan_to_num(df.loc[mask, 'ca1'] * df.loc[mask, 'g1'])  )
+    df.loc[mask, 'afe3o4_1'] = (np.nan_to_num(df.loc[mask, 'fe3o4_1'] * df.loc[mask, 'g1'])  )
+    
+    mask = (df[var_break] == brk) & (df['g2'] > 0)
+    
+    df.loc[mask, 'afe2'] = (np.nan_to_num(df.loc[mask, 'fe2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'asi2'] = (np.nan_to_num(df.loc[mask, 'si2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'ap2'] = (np.nan_to_num(df.loc[mask, 'p2'] * df.loc[mask, 'g2'])    )
+    df.loc[mask, 'aal2'] = (np.nan_to_num(df.loc[mask, 'al2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'amn2'] = (np.nan_to_num(df.loc[mask, 'mn2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'apf2'] = (np.nan_to_num(df.loc[mask, 'pf2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'ati2'] = (np.nan_to_num(df.loc[mask, 'ti2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'amg2'] = (np.nan_to_num(df.loc[mask, 'mg2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'aca2'] = (np.nan_to_num(df.loc[mask, 'ca2'] * df.loc[mask, 'g2'])  )
+    df.loc[mask, 'afe3o4_2'] = (np.nan_to_num(df.loc[mask, 'fe3o4_2'] * df.loc[mask, 'g2'])  )
+    
+    mask = (df[var_break] == brk) & (df['g3'] > 0)
+
+    df.loc[mask, 'afe3'] = (np.nan_to_num(df.loc[mask, 'fe3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'asi3'] = (np.nan_to_num(df.loc[mask, 'si3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'ap3'] = (np.nan_to_num(df.loc[mask, 'p3'] * df.loc[mask, 'g3'])    )
+    df.loc[mask, 'aal3'] = (np.nan_to_num(df.loc[mask, 'al3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'amn3'] = (np.nan_to_num(df.loc[mask, 'mn3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'apf3'] = (np.nan_to_num(df.loc[mask, 'pf3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'ati3'] = (np.nan_to_num(df.loc[mask, 'ti3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'amg3'] =(np.nan_to_num( df.loc[mask, 'mg3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'aca3'] = (np.nan_to_num(df.loc[mask, 'ca3'] * df.loc[mask, 'g3'])  )
+    df.loc[mask, 'afe3o4_3'] = (np.nan_to_num(df.loc[mask, 'fe3o4_3'] * df.loc[mask, 'g3'])  )
+    
+    mask = (df[var_break] == brk) & (df['g4'] > 0)
+
+    df.loc[mask, 'afe4'] = (np.nan_to_num(df.loc[mask, 'fe4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'asi4'] = (np.nan_to_num(df.loc[mask, 'si4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'ap4'] =  (np.nan_to_num(df.loc[mask, 'p4'] * df.loc[mask, 'g4'] )  )
+    df.loc[mask, 'aal4'] = (np.nan_to_num(df.loc[mask, 'al4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'amn4'] = (np.nan_to_num(df.loc[mask, 'mn4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'apf4'] = (np.nan_to_num(df.loc[mask, 'pf4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'ati4'] = (np.nan_to_num(df.loc[mask, 'ti4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'amg4'] = (np.nan_to_num(df.loc[mask, 'mg4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'aca4'] = (np.nan_to_num(df.loc[mask, 'ca4'] * df.loc[mask, 'g4'])  )
+    df.loc[mask, 'afe3o4_4'] = (np.nan_to_num(df.loc[mask, 'fe3o4_4'] * df.loc[mask, 'g4'])  )
+    
+    # Zera onde retido nas faixas = 0
+    
+    mask = (df[var_break] == 'FX') & (df['g1'] == 0) 
+    
+    df.loc[mask, 'afe1'] = 0
+    df.loc[mask, 'asi1'] = 0
+    df.loc[mask, 'ap1']  = 0
+    df.loc[mask, 'aal1'] = 0
+    df.loc[mask, 'amn1'] = 0
+    df.loc[mask, 'apf1'] = 0
+    df.loc[mask, 'ati1'] = 0
+    df.loc[mask, 'amg1'] = 0
+    df.loc[mask, 'aca1'] = 0
+    df.loc[mask, 'afe3o4_1'] = 0
+    
+    mask = (df[var_break] == 'FX') & (df['g2'] == 0)
+    
+    df.loc[mask, 'afe2'] = 0
+    df.loc[mask, 'asi2'] = 0
+    df.loc[mask, 'ap2']  = 0 
+    df.loc[mask, 'aal2'] = 0
+    df.loc[mask, 'amn2'] = 0
+    df.loc[mask, 'apf2'] = 0
+    df.loc[mask, 'ati2'] = 0
+    df.loc[mask, 'amg2'] = 0
+    df.loc[mask, 'aca2'] = 0
+    df.loc[mask, 'afe3o4_2'] = 0
+    
+    mask = (df[var_break] == 'FX') & (df['g3'] == 0)
+
+    df.loc[mask, 'afe3'] = 0
+    df.loc[mask, 'asi3'] = 0
+    df.loc[mask, 'ap3']  = 0
+    df.loc[mask, 'aal3'] = 0
+    df.loc[mask, 'amn3'] = 0
+    df.loc[mask, 'apf3'] = 0
+    df.loc[mask, 'ati3'] = 0
+    df.loc[mask, 'amg3'] = 0
+    df.loc[mask, 'aca3'] = 0
+    df.loc[mask, 'afe3o4_3'] = 0
+    
+    mask = (df[var_break] == 'FX') & (df['g4'] == 0)
+
+    df.loc[mask, 'afe4'] = 0
+    df.loc[mask, 'asi4'] = 0
+    df.loc[mask, 'ap4']  = 0
+    df.loc[mask, 'aal4'] = 0
+    df.loc[mask, 'amn4'] = 0
+    df.loc[mask, 'apf4'] = 0
+    df.loc[mask, 'ati4'] = 0
+    df.loc[mask, 'amg4'] = 0
+    df.loc[mask, 'aca4'] = 0
+    df.loc[mask, 'afe3o4_4'] = 0
+    
+    df = df.rename(columns=str.upper)
+    
+    return df
